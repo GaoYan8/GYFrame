3c60e160ba22982c772412d2ed2455e92cbc9abc
高炎 <yan.gao@zarltech.com>
2019-04-16 16:46:01 +0800

    up

--- gylibrary/src/main/java/com/gy/gylibrary/view/recyclerview/GYRecyclerViewEmptySupport.java
+++ gylibrary/src/main/java/com/gy/gylibrary/view/recyclerview/GYRecyclerViewEmptySupport.java
@@ -1,8 +1,6 @@
 package com.gy.gylibrary.view.recyclerview;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,68 +17,67 @@ import com.gy.gylibrary.utils.LogUtils;
  */
 public class GYRecyclerViewEmptySupport extends RecyclerView {
 
+    private View emptyView;
+    private static final String TAG = "EmptyRecyclerView";
+
+    final private AdapterDataObserver observer = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            LogUtils.i(TAG, "onItemRangeInserted" + itemCount);
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            checkIfEmpty();
+        }
+    };
+
     public GYRecyclerViewEmptySupport(Context context) {
         super(context);
     }
 
-    public GYRecyclerViewEmptySupport(Context context, @Nullable AttributeSet attrs) {
+    public GYRecyclerViewEmptySupport(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public GYRecyclerViewEmptySupport(Context context, @Nullable AttributeSet attrs, int defStyle) {
+    public GYRecyclerViewEmptySupport(Context context, AttributeSet attrs,
+                                      int defStyle) {
         super(context, attrs, defStyle);
     }
 
-    private static final String TAG = "RecyclerViewEmptySupport";
-    /**
-     * 当数据为空时展示的View
-     */
-    private View mEmptyView;
-    /**
-     * 创建一个观察者
-     * *为什么要在onChanged里面写？
-     * * 因为每次notifyDataChanged的时候，系统都会调用这个观察者的onChange函数
-     * * 我们大可以在这个观察者这里判断我们的逻辑，就是显示隐藏
-     */
-    private AdapterDataObserver emptyObserver = new AdapterDataObserver() {
-        @SuppressLint("LongLogTag")
-        @Override
-        public void onChanged() {
-            LogUtils.i(TAG, "onChanged: 000");
-            Adapter<?> adapter = getAdapter(); //这种写发跟之前我们之前看到的ListView的是一样的，判断数据为空否，再进行显示或者隐藏
-            if (adapter != null && mEmptyView != null) {
-                if (adapter.getItemCount() == 0) {
-                    mEmptyView.setVisibility(View.VISIBLE);
-                    GYRecyclerViewEmptySupport.this.setVisibility(View.GONE);
-                } else {
-                    mEmptyView.setVisibility(View.GONE);
-                    GYRecyclerViewEmptySupport.this.setVisibility(View.VISIBLE);
-                }
-            }
+    private void checkIfEmpty() {
+        if (emptyView != null && getAdapter() != null) {
+            final boolean emptyViewVisible =
+                    getAdapter().getItemCount() == 0;
+            emptyView.setVisibility(emptyViewVisible ? VISIBLE : GONE);
+            setVisibility(emptyViewVisible ? GONE : VISIBLE);
         }
-    };
-
-
-    /**
-     * * @param emptyView 展示的空view
-     */
-    public void setEmptyView(View emptyView) {
-        mEmptyView = emptyView;
     }
 
-    @SuppressLint("LongLogTag")
     @Override
     public void setAdapter(Adapter adapter) {
-        super.setAdapter(adapter);
-        LogUtils.i(TAG, "setAdapter: adapter::" + adapter);
-        if (adapter != null) {
-            //这里用了观察者模式，同时把这个观察者添加进去，
-            // 至于这个模式怎么用，谷歌一下，不多讲了，因为这个涉及到了Adapter的一些原理，感兴趣可以点进去看看源码，还是受益匪浅的
-            adapter.registerAdapterDataObserver(emptyObserver);
+        final Adapter oldAdapter = getAdapter();
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(observer);
         }
-        //当setAdapter的时候也调一次（实际上，经我粗略验证，不添加貌似也可以。不行就给添上呗，多大事嘛）
-        emptyObserver.onChanged();
+        super.setAdapter(adapter);
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(observer);
+        }
+
+        checkIfEmpty();
     }
 
+    //设置没有内容时，提示用户的空布局
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+        checkIfEmpty();
+    }
 
 }
